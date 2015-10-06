876770a836742db27f80078297d5bb3e9caa053a
Matt Todd <chiology@gmail.com>
2015-10-06 00:26:21 -0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  get  "/samples" => 'samples#index', :as => :samples
   post "/samples" => "samples#create"
-
-  root :to => 'assets#index'
-  get 'assets/index'
+  root :to => redirect('/samples')
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
