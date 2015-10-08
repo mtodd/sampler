a0462454fef518d48c9f86fcd68fa924f65e749a
Matt Todd <chiology@gmail.com>
2015-10-07 21:32:46 -0700

    Generate unique tokens

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,5 +1,11 @@
 class Study
-  def self.generate_unique_token
-    "new"
+  def self.generate_unique_token(initial_token = nil)
+    token = initial_token || generate_token
+    token = generate_token while Sample.where(:study_token => token).any?
+    token
+  end
+
+  def self.generate_token
+    SecureRandom.hex(8)
   end
 end
